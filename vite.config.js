3f4ed2dde2b60d44524d1025f8b8e0b284be6527
inqupa <inqupa.gmail.com>
2026-04-25 19:21:43 +0530

    refactor: dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [
-    basicSsl()
-  ],
   server: {
-    https: true,
     port: 3000,
     open: true,
     proxy: {
